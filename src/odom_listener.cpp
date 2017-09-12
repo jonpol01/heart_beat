e443fdeab516dc287a6bd289828c87ce71ddbbb6
logiler <i2a_tech@seaos.co.jp>
2017-09-12 18:03:00 +0900

    goal,pose,odom subscriber and callbacks

--- src/odom_listener.cpp
+++ src/odom_listener.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
+#include "geometry_msgs/PoseStamped.h"
 
 /**
  * This tutorial demonstrates simple receipt of position and speed of the Evarobot over the ROS system.
@@ -9,7 +10,8 @@
  * Callback function executes when new topic data comes.
  * Task of the callback function is to print data to screen.
  */
-void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   ROS_INFO("Seq: [%d]", msg->header.seq);
   ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
@@ -17,8 +19,22 @@ void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
   ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
 }
 
+void poseCallback(const geometry_msgs::Pose::ConstPtr& msg)
+{
+  ROS_INFO("Point A-> x: [%2f], y: [%2f], z: [%2f]", msg->position.x, msg->position.y, msg->position.z);
+}
+
+void goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+//  ROS_INFO("Point B-> x: [%f], y: [%f], z: [%f]", msg->position.x, msg->position.y, msg->position.z);
+  ROS_INFO("Point B-> x: [%f]", msg->pose.position.x);
+}
+
 int main(int argc, char **argv)
 {
+
+  double controller_frequency_;
+
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -38,6 +54,9 @@ int main(int argc, char **argv)
    */
   ros::NodeHandle n;
 
+  //basic parameters
+  n.param("controller_frequency", controller_frequency_, 0.5); //5x per second
+
   /**
    * The subscribe() call is how you tell ROS that you want to receive messages
    * on a given topic.  This invokes a call to the ROS
@@ -53,14 +72,21 @@ int main(int argc, char **argv)
    * is the number of messages that will be buffered up before beginning to throw
    * away the oldest ones.
    */
-  ros::Subscriber sub = n.subscribe("odom", 1000, chatterCallback);
+//  ros::Subscriber sub = n.subscribe("odom", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("robot_pose", 1000, poseCallback);
+  ros::Subscriber sub2 = n.subscribe("/current_goal", 1000, goalCallback);
 
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
    * callbacks will be called from within this thread (the main one).  ros::spin()
    * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
    */
-  ros::spin();
+//  ros::spin();
+  ros::Rate loop_rate(controller_frequency_);
+  while(ros::ok()){
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
   return 0;
 }
