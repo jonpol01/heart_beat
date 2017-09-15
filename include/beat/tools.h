1451946e890668de2c4fde88ccd9b3ec5e3b041e
jonpol01 <johnpaul@soliva.me>
2017-09-15 15:11:11 +0900

    test env

--- include/beat/tools.h
+++ include/beat/tools.h
@@ -1,1 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/PoseStamped.h"
+
+namespace Pathfinder
+{
+using namespace std;
+
+class util
+{
+  public:
+    void poseCallback(const msg);
+  private:
+};
+
+/**
+ * Callback function executes when new topic data comes.
+ * Task of the callback function is to print data to screen.
+ */
+void util::poseCallback(const geometry_msgs::Pose::ConstPtr& msg)
+{
+  ROS_INFO("Point A-> x: [%2f], y: [%2f], z: [%2f]", msg->position.x, msg->position.y, msg->position.z);
+}
+
+ /*
+ void util::odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+ {
+   ROS_INFO("Seq: [%d]", msg->header.seq);
+   ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
+   ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+   ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
+ }
+ 
+ void util::goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+ {
+ //  ROS_INFO("Point B-> x: [%f], y: [%f], z: [%f]", msg->position.x, msg->position.y, msg->position.z);
+   ROS_INFO("Point B-> x: [%f]", msg->pose.position.x);
+ }
+ */
+} //end namespace
--- src/pose_listener.cpp
+++ src/pose_listener.cpp
@@ -1,39 +1,11 @@
 #include "ros/ros.h"
-#include "nav_msgs/Odometry.h"
-#include "geometry_msgs/PoseStamped.h"
 
 #include "beat/tools.h"
-/**
- * This tutorial demonstrates simple receipt of position and speed of the Evarobot over the ROS system.
- */
 
-/**
- * Callback function executes when new topic data comes.
- * Task of the callback function is to print data to screen.
- */
-
-void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
-{
-  ROS_INFO("Seq: [%d]", msg->header.seq);
-  ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
-  ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
-  ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
-}
-
-void poseCallback(const geometry_msgs::Pose::ConstPtr& msg)
-{
-  ROS_INFO("Point A-> x: [%2f], y: [%2f], z: [%2f]", msg->position.x, msg->position.y, msg->position.z);
-}
-
-void goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
-{
-//  ROS_INFO("Point B-> x: [%f], y: [%f], z: [%f]", msg->position.x, msg->position.y, msg->position.z);
-  ROS_INFO("Point B-> x: [%f]", msg->pose.position.x);
-}
 
 int main(int argc, char **argv)
 {
-
+  Pathfinder::util util;
   double controller_frequency_;
 
   /**
@@ -74,7 +46,7 @@ int main(int argc, char **argv)
    * away the oldest ones.
    */
   //ros::Subscriber sub = n.subscribe("odom", 1000, chatterCallback);
-  ros::Subscriber sub = n.subscribe("robot_pose", 1000, poseCallback);
+  ros::Subscriber sub = n.subscribe("robot_pose", 1000, util.poseCallback);
   //ros::Subscriber sub2 = n.subscribe("/current_goal", 1000, goalCallback);
 
   /**
